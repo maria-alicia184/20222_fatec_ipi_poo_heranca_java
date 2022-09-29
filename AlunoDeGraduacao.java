96658d04295d71d8bad049c67e21eaf23c82d15a
unknown <aluno@L1M17.IPIRANGA.FATEC>
2022-09-29 16:00:28 -0300

    heranca(apostila05): fim da listagem 1.4.1

---
+++ AlunoDeGraduacao.java
@@ -0,0 +1,6 @@
+//AlunoDeGraduacao É-UM Pessoa
+public class AlunoDeGraduacao 
+    extends Pessoa{
+        double nota1, nota2, notaFinal;
+    }
+
---
+++ TesteHeranca1.java
@@ -0,0 +1,9 @@
+public class TesteHeranca1 {
+    public static void main(String [] args){
+        //vejamos se alunos de graduação tem nome e idade
+        AlunoDeGraduacao aluno = new AlunoDeGraduacao();
+        aluno.nome = "João";
+        aluno.idade = 17;
+        System.out.printf("Nome: %s, idade: %d\n", aluno.nome, aluno.idade);
+    }
+}
